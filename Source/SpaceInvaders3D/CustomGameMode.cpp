43ab843260453a0b45ee26f52e348644f8ba56ee
Bardia Hassanzadeh <bardia@ualberta.ca>
2023-03-13 04:17:31 -0700

    Commented on the Custom Game Mode Class

--- Source/SpaceInvaders3D/CustomGameMode.cpp
+++ Source/SpaceInvaders3D/CustomGameMode.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-// The Game Mode Class
+// The Game Mode Class for the Project
 
 #include "CustomGameMode.h"
 #include "CustomGameState.h"
